926979aa24053f279a7a1b4904dbc19044dc4708
Mrfaheem4 <mrfaheem010@gmail.com>
2026-02-28 20:01:03 +0500

    animations

--- src/Components/Current.jsx
+++ src/Components/Current.jsx
@@ -1,10 +1,19 @@
+import TextType from "../Animations/TextType";
+
 export default function Current({ cityName, countryName }) {
   return (
-    <div className="bg-gray-100 px-8 py-6 border-white border-2 ">
-      <h2 className="text-6xl font-semi text-black ">
-        {cityName},<br />
-        {countryName}
-      </h2>
+    <div className="bg-gray-100 px-8 py-6 border-white border-2">
+      <TextType
+        key={cityName}
+        text={[`${cityName}, ${countryName}`]}
+        typingSpeed={40}
+        deletingSpeed={null}
+        pauseDuration={100}
+        showCursor={false}
+        cursorCharacter="|"
+        className="text-6xl font-semi text-black"
+        loop={false}
+      />
     </div>
   );
 }
